9d8bb827c47267dd35ae4a6d0dcb8b85f1881d98
Mariley20 <mariley2018@gmail.com>
2017-11-14 14:42:02 -0500

    await fech

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import { NavLink } from 'react-router-dom';
 import './css/app.css';
 import {search, search2 } from './actions';
-export const Home = ({items}) => {
+export const Home = ({items, planets}) => {
     const itemsss = items.map((element, index) => {
         return(
             <p key={index}  >{element}</p>
@@ -13,7 +13,7 @@ return (
         <input type='text' />
         <button onClick={search}>Buscar</button>
         <div>
-            {itemsss}
+            {planets}
         </div>
     </div>
 )
--- src/actions.js
+++ src/actions.js
@@ -2,34 +2,27 @@ import store from "./store";
 // import url from './data/earth-like-results.json'
 
 export async function search() {
-    const url = "data/earth-like-results.json";
-    fetch(url)
-        .then(res => res.json())
-        .then(res => {
-            console.log('res', res)
-            store.setState({
-                items: res.results
-            })
-        });
+    // const url = "data/earth-like-results.json";
+    // fetch(url)
+    //     .then(res => res.json())
+    //     .then(res => {
+    //         store.setState({
+    //             items: res.results,
+    //         })
+    //     });
 
-    const cloneItem = [...store.getState().items]
-    cloneItem.map(
-        (item, index) => {
-            console.log('i', item, 'in', index)
-            fetch(item.toString())
-                .then(response => response.json())
-                .then(response => {
-                    console.log('response', response)
-                    let dataPlanet = [...store.getState().planets];
-                    dataPlanet.push(response);
-                    store.setState({
-                        planets: dataPlanet,
-                    })
-                })
-        }
-    )
-    console.log('items', store.getState().items)
-    console.log('planets', store.getState().planets)
+
+    const res = await fetch("data/earth-like-results.json");
+    const generalJSON = await res.json();
+    let list = addPlanets(generalJSON.results);
+    console.log('list fir-37152', list)
+    store.setState({
+        planets: list,
+    })
+}
+
+function addPlanets(list) {
+    return Promise.all(list.map(item => fetch(item).then(res => res.json())))
 
 }
 
